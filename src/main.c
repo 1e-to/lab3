e6f35599b926c5f32cdc1133c0066868bb6a7212
totoro4 <totmeninal@mail.ru>
2016-06-10 20:00:46 +0600

    Added output

--- src/main.c
+++ src/main.c
@@ -24,5 +24,13 @@ int main(int argc, char *argv[])
 	int amount;
 	roots(a, b, c, &amount, &x1, &x2);
 
+	if (amount == 0) {
+		printf("This equation has noo roots\n");
+	} else if (amount == 1) {
+		printf("There is only one root:\n%f\n", x1);
+	} else if (amount == 2) {
+		printf("There are two roots:\nx1 = %f\tx2 = %f\n", x1, x2);
+	}
+
 	return EXIT_SUCCESS;
 }
